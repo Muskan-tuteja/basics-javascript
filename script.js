0b71ce5e010edd69ecb80c88ce67a15742c7e9e7
muskan <muskantuteja714@gmail.com>
2025-07-15 12:05:26 +0530

    12:7

---
+++ script.js
@@ -0,0 +1,34 @@
+// var let const
+
+// var
+// window me add hota hai
+// same name hoge to error ni aya ga  
+// var a = 2;
+// var a =4
+// console.log(a)
+
+
+// let (change value)
+let a = 23
+let name="mahii"
+console.log("name")
+console.log(a)
+
+
+// const(not change value)
+const namee="muskan"
+const last="mahi"
+
+
+// arthimetic 
+// add and multi
+var c = 45
+var b= 56
+var f=23
+e = c+b*f
+console.log(e)
+
+
+
+
+
