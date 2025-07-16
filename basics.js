3df86ce462448fb0325b45dfbab368c00d26ea74
muskan <muskantuteja714@gmail.com>
2025-07-16 14:09:11 +0530

    16/7/25

---
+++ basics.js
@@ -0,0 +1,31 @@
+// scope (global,block,functional)
+
+// global scope
+var a=20;
+
+// functional scope
+function abcd(){
+  let b =20
+}
+// block scope
+{
+
+}
+
+// reassignment ,redeclaration
+// var a =12;
+// a=34;
+// var a =23
+
+
+
+// let b =34
+// b=78
+
+// let b =68 not declaration
+
+// temporal dead zone(utna area jitna main js lo pata to hai ki variable exist karta hai par wo apko value nahi de sakta  )
+
+console.log(a)
+
+let a = 20
--- script.js
+++ script.js
@@ -9,10 +9,10 @@
 
 
 // let (change value)
-let a = 23
-let name="mahii"
-console.log("name")
-console.log(a)
+// let a = 23
+// let name="mahii"
+// console.log("name")
+// console.log(a)
 
 
 // const(not change value)
@@ -20,13 +20,155 @@ const namee="muskan"
 const last="mahi"
 
 
-// arthimetic 
-// add and multi
-var c = 45
-var b= 56
-var f=23
-e = c+b*f
-console.log(e)
+// arthimetic operators 
+// additions
+// let c = 45
+// let b= 56
+// let f=23
+// let e = c+b+f
+// console.log(e)
+
+// substraction
+// let c = 45
+// let b= 56
+// let f=23
+// let e = c-b+f
+// console.log(e)
+
+// multiplaction
+// let a = 32
+// let b= 45
+// c= a*b
+// console.log(c)
+
+// division
+// let a = 32
+// let b= 45
+// c= a/b
+// console.log(c)
+
+// modulus
+// let a = 32
+// let b= 45
+// c= a%b
+// console.log(c)
+
+// exponentation
+// let a = 32
+// let b= 45
+// c= a**b
+// console.log(c)
+
+// Unary Operators
+
+// let a=15
+// let b=20
+// let c = a++ + ++b
+// console.log(c)
+
+
+// assignment opertor
+// let a=10
+// let b=20
+// let c=a=b
+// console.log(c)
+
+
+// add and asign
+// let a=10
+// let b=20
+// let c=a+=b
+// console.log(c)
+
+// subtract and assign
+// let a=10
+// let b=20
+// let c=a-=b
+// console.log(c)
+
+
+
+// comparison opertors
+// let a =10
+// let b= 20;
+// let c=a==b
+// console.log(c)
+
+// strict comparsion
+// let a =10
+// let b= 20;
+// let c=a===b
+// console.log(c)
+
+// not equal
+// let a =10
+// let b= 20;
+// let c= a!=b
+// console.log(c)
+
+// not equal
+// let a =10
+// let b= 20;
+// let c= a!==b
+// console.log(c)
+
+// greater than
+// let a =10
+// let b= 20;
+// let c= a>b
+// console.log(c)
+
+// less than
+// let a =10
+// let b= 20;
+// let c= a<b
+// console.log(c)
+
+
+// bitwise opertor
+// & and (dono me se koi bi ik falue hoga to vo faluse hoga )
+// let a =1
+// let b= 1;
+// let c= a&b
+// console.log(c)
+
+
+// bitwise or
+// dono me se koi bi ik true hoga to vo true hoga (f,f=f)
+// let a =1
+// let b= 1;
+// let c= a|b
+// console.log(c)
+
+
+// bitwise xor
+// let a =0
+// let b= 1;
+// let c= a^b
+// console.log(c)
+
+
+
+// (70+70=140,140+140=280)
+// let a=70
+// let b=2
+// let c= a<<b
+// console.log(c)
+
+// 10=5,5=2.5
+// let a=10
+// let b=2
+// let c= a>>b
+// console.log(c)
+
+
+
+
+
+
+
+
+
 
 
 
