5b4570d111b671ffd9c126f54ba34a29ee580978
muskan <muskantuteja714@gmail.com>
2025-07-18 23:58:07 +0530

    18/7/25

--- basics.js
+++ basics.js
@@ -1,12 +1,12 @@
 // scope (global,block,functional)
 
 // global scope
-var a=20;
+// var a=20;
 
 // functional scope
-function abcd(){
-  let b =20
-}
+// function abcd(){
+//   let b =20
+// }
 // block scope
 {
 
@@ -29,3 +29,79 @@ function abcd(){
 // console.log(a)
 
 // let a = 20
+
+
+// .....Hosting impact per type....
+
+
+
+// console.log(a)
+// let a = 12
+
+// var=>hoist=>undefined
+// let=>hoist=>x(initilization)
+// const=>hoist=>x(intilization)
+
+// practice question and answer(hoisting)
+// console.log(nm)
+// var nm="muskan"
+
+// console.log(age)
+// let age =23;
+
+
+// let a = 10 
+// {
+//   let a =20;
+//   console.log("inside:",a);
+
+// }
+// console.log("outside:",a)
+
+// if(true){
+//   var a= 1
+//   let b =2
+// }
+// let curly barces ki  bhar use ni kar sakte hai
+// var ka curky barces ke ander is liya use ho raha h kuki koi (function create ni hai )
+// console.log(a)
+// console.log(b)
+
+
+// why const allows changing object properties
+// const person ={name: "muskan"}
+// person.name = "isha"
+// person = {}/
+
+
+// data type
+// primitives
+
+// symbol => unique immutable value
+
+let ut1 = "muskan"
+let ut2= "muskan"
+
+// type coercion => concept jismein apka ek type automatically convert hojaayega
+
+// 0 false "" null undefined NaN document.all => false
+
+//type of NaN =>Number ku hai . NaN => ik falied number of opeation 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
