af57152940377996b86a26b7cdc3e03d003cd04f
muskan <muskantuteja714@gmail.com>
2025-07-28 23:34:56 +0530

    28/7/25

--- pratices.js
+++ pratices.js
@@ -1,38 +1,132 @@
-function Name(muskan){
-  console.log("hello"+muskan)
-}
-Name("musu")
+// function Name(muskan){
+//   console.log("hello"+muskan)
+// }
+// Name("musu")
+// function add(a,b){
+//   console.log(a+b)
+// }
+// add(10,20)
 
-function sum(a,b){
-  return a+b;
+// function sum(a,b){
+//   return a+b;
   
-}
+// }
 
-console.log(sum(12,35))
+// console.log(sum(12,35))
 
-function isEven(num){
-return num%2 ==0
-}
-console.log(isEven(6))
+// function isEven(num){
+// return num%2 ==0
+// }
+// console.log(isEven(6))
 
-function isOdd(num){
-  return num % 3===0
-}
-console.log(isOdd(5))
-
-
-function max(a,b){
-  return a>b ? a:b
-}
-console.log(max(10,20))
-
-function countLength(str){
-  return str.length
-}
-console.log(countLength("muskan + mahi"))
+// function isOdd(num){
+//   return num % 3===0
+// }
+// console.log(isOdd(5))
 
 
-function reverseString(str){
-  return str.split('').reverse().join('')
-}
-console.log(reverseString('hello'))
+// function max(a,b){
+//   return a>b ? a:b
+// }
+// console.log(max(10,20))
+
+// function countLength(str){
+//   return str.length
+// }
+// console.log(countLength("muskan + mahi"))
+
+
+// function reverseString(str){
+//   return str.split('').reverse().join('')
+// }
+// console.log(reverseString('hello'))
+
+
+
+
+// function sum(){
+//   console.log(a+b)
+// }
+// sum()
+
+// function addNumber(a,b){
+//   return a+b
+// }
+// console.log(addNumber(10,29))
+
+
+// function isEven(a){
+//   return a%2 === 0
+// }
+// console.log(isEven(12))
+// console.log(isEven(15))
+
+
+// function fetFirstChar(Name){
+//   return Name.charAt(1)
+// }
+// console.log(fetFirstChar("muskan"))
+
+// function findLargest(a,b,c){
+//   return Math.max(a,b,c)
+// }
+// console.log(findLargest(10,20,25))
+
+
+// function findSmallest(a,b,c){
+//   return Math.min(a,b,c)
+// }
+// console.log(findSmallest(10,0,20))
+
+
+// function findMiddle(a,b,c,d,e){
+//   return Math.imul(a,b,c,d,e)
+// }
+// console.log(findMiddle(10,20,30,2,4))
+
+
+// function countVowels(Name){
+//   let count =0
+//   let vowels ="aeiou"
+  
+//   for(let i =0; i<Name.length; i++){
+//     if(vowels.includes(Name[i])){
+//       count++
+//     }
+//   }
+//   return count
+// }
+// console.log(countVowels("isha"))
+
+
+
+// arror function
+// const outer = (a) =>{
+//   return a** 2
+// }
+// console.log(outer(5))
+// const outer = (a,b) =>{
+//   return a+b
+// }
+// console.log(outer(5,8))
+
+
+// function expression
+
+// let fnc = function (){
+//   console.log("mudskan")
+// }
+// fnc()
+
+// const add =function(a,b){
+//   console.log(a+b)
+// }
+// add(10,20)
+
+
+// function declaration
+// function sum (a,b){
+// console.log(a+b)
+// }
+// sum(10,20)
+
