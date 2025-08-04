77074e8615a839b716a2242c541f79e14c663dda
muskan <muskantuteja714@gmail.com>
2025-08-04 09:32:45 +0530

    4/8/25

---
+++ object.js
@@ -0,0 +1,51 @@
+// let obj = {
+//   name: "muskan",
+//   age:23,
+//   rollno: 21
+// }
+
+// console.log(obj.name)
+// console.log(obj.age)
+
+
+
+
+// let student = {
+//   name: "Muskan",
+//   "home address": "Delhi",
+//   age: 22
+// };
+
+// console.log(student["name"]);         
+// console.log(student["home address"]);  
+
+
+
+// let marks = {
+//   name :"mahi",
+//   class : "cse",
+//   rollno:23,
+//   marks:83
+
+// }
+// console.log(marks.marks)
+// console.log(marks["class"])
+
+
+// let user = {
+//   name: "Muskan",
+//   address: {
+//     city: "Delhi",
+//     pin: 110001,
+//     location: {
+//       lat: 28.6139,
+//       lng: 77.2090
+//     }
+//   }
+// };
+
+// console.log(user.address.location.lng)
+
+// console.log(user["address"]["pin"])
+
+
