9583515dde8a9f5814ffd667e4f0284b4bf6cedd
muskan <muskantuteja714@gmail.com>
2025-08-04 13:53:10 +0530

    4/08/25

--- object.js
+++ object.js
@@ -44,8 +44,159 @@
 //   }
 // };
 
+
+//deep accesss bolte h ise 
 // console.log(user.address.location.lng)
 
 // console.log(user["address"]["pin"])
+
+
+// short from access karne h to
+// let{lat,lng}= user.address.location
+
+
+
+// looping
+// for in
+
+// let obj = {
+//   name : "muahi",
+//   age: 23,
+// email:"hgbnbjgn mnkj"
+
+// }
+// for(let key in obj){
+//   console.log(key,obj[key])
+// }
+
+
+// object key
+
+
+// let person = {
+//   name: "Muskan",
+//   age: 22,
+//   city: "Delhi"
+// };
+
+// Object.keys(person).forEach(function(key) {
+//   console.log(key + ":", person[key]);
+// });
+
+
+// object.enteries()
+
+// let person = {
+//   name : "muskan",
+//   age: 21,
+//   city:"hansi"
+
+// };
+// for(let[key,value]of Object.entries(person)){
+//   console.log(key + ":" ,value)
+// }
+
+// spread opertor
+
+// let person = {
+//   name : "muskan",
+//   age: 21,
+//   city:"hansi"
+
+// };
+// let obj2 = {...person}
+// console.log(person)
+
+// deep clone
+
+// let obj = {
+//   name : "muskan",
+//   age:23,
+//   emaill:"bhfnk@hnnh",
+//   address :{
+//     city : "mkihi"
+//   }
+// }
+// let obj2 = JSON.parse(JSON.stringify(obj))
+
+
+// optional chaining
+
+// let obj = {
+//   name : "muskan",
+//   age:23,
+//   emaill:"bhfnk@hnnh",
+//   addressss :{
+//     city : "mkihi"
+//   }
+// }
+// console.log(obj?.address?.city)
+
+
+
+// pretices
+
+// let obj={
+//   name:"muskani",
+//   roll:34,
+//   isEnrolled:45
+
+// }
+// console.log(obj.roll)
+
+// const user = {
+//   "first-name":"mkil"
+// }
+// user["first-name"]
+// console.log(user)
+
+
+// const user = {
+//   age : 23
+// }
+// let key = "age"
+// user[key]
+
+// const Locations = {
+//   city:"mkijo",
+//   coordinates :{
+//     late:23,
+//     lng:90
+
+//   }
+// }
+// Locations.coordinates.lng
+
+
+
+// const user = {
+//   "first-name" : "musjki",
+// }
+
+// let {"first-name": firstName} = user
+
+// const course = {
+//   title : "java",
+//   duration : "4 weeks"
+// }
+
+// for(let key in course){
+//   console.log(key)
+// }
+
+
+// const course = {
+//   title : "java",
+//   duration : "4weeks"
+// }
+
+// Object.entries(course).forEach(function(val){
+//   console.log(val[0] + ":" + val[1])
+// })
+
+// const obj1 = {info:{score:34}}
+// const clone = {...obj1}
+// clone.info.score= 100;
+// console.log(obj1.info.score)
 
 
---
+++ str.js
@@ -0,0 +1,28 @@
+// let str = "Muskan";
+// console.log(str.length);
+
+// let str = "hello";
+// console.log(str.toUpperCase()); 
+
+// let str = "HELLO";
+// console.log(str.toLowerCase());
+
+// let str = "Muskan";
+// console.log(str.charAt(3));
+
+// let str = "Hello world";
+// console.log(str.indexOf("world"))
+
+// let str = "JavaScript";
+// console.log(str.includes("Script")); 
+
+// let str = "Good morning";
+// let newStr = str.replace("morning", "evening");
+// console.log(newStr);
+
+// let name = "Muskan";
+// let msg = `Welcome, ${name}!`;
+// console.log(msg);
+
+let str = "   Hello   ";
+console.log(str.trim());
