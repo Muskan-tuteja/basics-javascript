b35cca1b7bb70d9d2d2adc6f55fd9aa2f759a135
muskan <muskantuteja714@gmail.com>
2025-08-11 23:43:43 +0530

    event

---
+++ event.js
@@ -0,0 +1,35 @@
+// let h1 = document.querySelector("h1")
+
+// h1.addEventListener("click", function(){
+//   h1.style.color = "blue"
+// })
+
+// let but = document.querySelector("button")
+
+// but.addEventListener("dblclick" , function(){
+//   but.style.color = "green"
+//   but.style.backgroundColor = "yellow"
+// })
+
+// let but = document.querySelector("button")
+
+// but.addEventListener("click",function(){
+//   alert("button clicked")
+// })
+// let but = document.querySelector("button").removeEventListener("click",function(){
+//   alert("button clicked")
+// })
+
+// function handleClick() {
+//   alert("button click");
+// }
+
+// const btn = document.querySelector("button");
+// btn.addEventListener("dblclick", handleClick);
+// btn.removeEventListener("dblclick", handleClick);
+
+// let input = document.querySelector("input")
+
+// input.addEventListener("input",fuction(){
+
+// })
