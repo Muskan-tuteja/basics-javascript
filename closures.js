2fb9f819c71cb1d570a4c1074930abfbb1e892c1
muskan <muskantuteja714@gmail.com>
2025-08-21 16:02:22 +0530

    21/8/25

---
+++ closures.js
@@ -0,0 +1,52 @@
+// function adcd (){
+//   let a =13
+//   return function(){
+//     console.log(a)
+//   }
+// }
+// let fnc = adcd()
+// fnc()
+
+
+// private counter
+
+// function counter(){
+//   let count = 0
+
+//   return function(){
+//     count++
+//     console.log(count)
+//   }
+// }
+// let c1 = counter()
+// c1()
+// c1()
+// c1()
+// c1()
+
+// let c2 = counter()
+// c2()
+// c2()
+// c2()
+// c2()
+// c2()
+
+
+// function clickLimmit(){
+//   let click = 0
+//   return function(){
+//     if(click<5){
+//       click++
+//       console.log(`clicked : ${click} times`)
+//     }else{
+//       console.error("LIMIT EXCEEDEDE.TRY AFTER SOME TIME")
+//     }
+//   }
+// }
+// let func = clickLimmit()
+// func()
+// func()
+// func()
+// func()
+// func()
+// func()
