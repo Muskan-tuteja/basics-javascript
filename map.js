fa0d20b5948a56536244f1d72b020def13ca8f00
muskan <muskantuteja714@gmail.com>
2025-11-08 23:15:46 +0530

    08/11/25

---
+++ map.js
@@ -0,0 +1,6 @@
+// let arr = [1,2,3,4,5,6]
+// let dounble = arr.map((item)=>item*2)
+// let arr2 = arr2.map((item)=>item == 5)
+// console.log(dounble)
+// console.log(arr)
+// console.log(arr2.type)
