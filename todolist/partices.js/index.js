229d8fb53f5feed95d4011f345a040c849d6fc76
muskan <muskantuteja714@gmail.com>
2025-11-09 23:24:50 +0530

    09/11/25

---
+++ todolist/partices.js/index.js
@@ -0,0 +1,92 @@
+// var $x=20
+// console.log($x)
+
+// var names = "muskan"
+// console.log(names)
+
+//  let x =10
+
+//  if(x>5){
+//   let y =20
+//   console.log(x)
+//  }
+//  console.log(y)
+
+// var x =2
+// var x=3
+// console.log(x)
+
+// let x=3
+// // let x=9
+// console.log(x)
+
+// console.log(city)
+// const city = "muskan"
+
+// {
+//   var a=3
+//   let b =9
+//   const c =4
+//   console.log(b)
+// console.log(c)
+// }
+// console.log(a)
+//  let b = 20(
+//   for(let b=0; b<3; b++){
+//     setTimeout(()=>console.log(b),1000)
+//   }
+
+// )
+
+// var q = 45
+// var e = (q + "muskan" + e)
+// console.log(typeof q)
+
+// console.log(typeof e)
+
+// let a ="muskan"
+// // b=56
+// b=a+78
+// console.log(typeof a)
+// console.log( a)
+// console.log( b)
+// console.log(typeof b)
+
+// const a = 23
+// const b =67 +"muskan"
+
+// console.log(b)
+
+// global scope
+// var city = "delhi"
+
+// function showcity(){
+//  let  abc = 67
+//  const edf = "mahi"
+//   console.log(city)
+//    console.log(abc)
+//   console.log(edf)
+
+// }
+// showcity()
+// console.log(city)
+
+// function scope
+// function exam() {
+//   var fs = "muskan";
+//   console.log(fs);
+// }
+// exam();
+
+function demo (){
+  if(true){
+    var a =10
+    let b =30
+    const c = 45
+     console.log(b)
+  console.log(c)
+  }
+  console.log(a)
+ 
+}
+demo()
