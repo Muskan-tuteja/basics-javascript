9cb200ef7900e847a6a13cea75aae14b6f387dc6
muskan <muskantuteja714@gmail.com>
2026-04-10 00:02:30 +0530

    DSA JS

--- DSAJS/script.js
+++ DSAJS/script.js
@@ -78,5 +78,18 @@
 // console.log()
 // }
 
-
+// let n =5
+// for(let i =1;i<=n;i++){
+//   for(let j =1; j<=n-i+1;j++){
+//     process.stdout.write("* ")
+//   }
+//   console.log()
+// }
+let n =5
+for(let i =1;i<=n;i++){
+  for(let j =1; j<=i;j++){
+    process.stdout.write("* ")
+  }
+  console.log()
+}
 
