b7690c6b0b67cfc3c9482dde9ba899e7e74e718b
muskan <muskantuteja714@gmail.com>
2026-04-09 17:36:43 +0530

    DSA JS

---
+++ DSAJS/script.js
@@ -0,0 +1,41 @@
+// let a = 20;
+// let b = "34"
+// let c = 45
+
+// console.log("sum of 10 and 20 is "  + (a+c)+b);
+// console.log("5"-5);
+// console.log("5"*5);
+// console.log("10" + 5 - 2);
+// console.log (typeof("5" + 3 * 2));
+
+// let age = Number( prompt("this is my age"))
+// // let age =  prompt("this is my age")
+// console.log(typeof age)
+
+// let a = 20
+// let b =10
+// let c 
+// console.log(c=a);
+// console.log(a=b);
+// console.log(b=c);
+
+// let a = 20
+// let b =30;
+// [a,b]=[b,a]
+// console.log(a,b);
+
+// let a = 12
+// let b =22 
+// console.log(Math.floor (a/b));
+
+// let a = 7
+// let b =2
+// console.log(typeof(a%b))
+
+// console.log(a==a);
+
+
+
+
+
+
