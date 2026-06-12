5842c183b6701185318bbea3ac8c57aa1d94c8d5
muskan <muskantuteja714@gmail.com>
2026-06-12 21:31:07 +0530

    basic js

---
+++ company.js
@@ -0,0 +1,73 @@
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+
+// let b = 40;
+//  b = 60;
+//  console.log(b);
+
+//  const v = 30;
+//   // v = 40;
+//  console.log(v);
+
+// function outer(){
+//   let count = 0;
+
+//   return function inner(){
+//     count++
+//     console.log(count);
+    
+//   }
+// }
+// const counter = outer();
+// counter();
+// counter();
+
+// counter();
+// counter();
+
+
+// function outer(){
+//   let count = 0;
+
+//   return function inner(){
+//     count++
+//     console.log(count);
+    
+//   }
+// }
+
+// let counter = outer()
+// counter()
+// counter()
+// counter()
+// counter()
+
+ 
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+// var a 
+// console.log(a);
+// a=10;
+
+
+// const a;
+// console.log(a);
+// const a = 10
+// // a = 29;
+// // console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
