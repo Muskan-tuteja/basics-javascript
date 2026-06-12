f09995a52bb692d6506d3b7cb1e500f5dee387c6
muskan <muskantuteja714@gmail.com>
2026-06-12 20:42:20 +0530

    dsa

--- DSAJS/script.js
+++ DSAJS/script.js
@@ -85,11 +85,83 @@
 //   }
 //   console.log()
 // }
+// let n =5
+// for(let i =1;i<=n;i++){
+//   for(let j =1; j<=i;j++){
+//     process.stdout.write("* ")
+//   }
+//   console.log()
+// }
+
+// let n = 5
+// for(let i =1;i<=n;i++){
+//   for(let j = 1; j<=n-i+1;j++){
+//     process.stdout.write("* ")
+//   }
+//   console.log();
+  
+// }
+
+// let n =5
+// for(let i =1; i<=n;i++){
+//   for(let j =1;j<=n-i;j++){
+//       process.stdout.write(" ")
+//   }
+//   for(let j =1;j<=i;j++){
+//       process.stdout.write(" *")
+//   }
+//   console.log()
+// }
+
+
 let n =5
-for(let i =1;i<=n;i++){
-  for(let j =1; j<=i;j++){
+for(let i=1;i<=n;i++){
+  for(let j =1;j<=n;j++){
     process.stdout.write("* ")
   }
   console.log()
 }
+// let n =5
+// for(let i=1;i<=n;i++){
+//   for(let j =1;j<=n-i+1;j++){
+//     process.stdout.write("* ")
+//   }
+//   console.log()
+// }
+// let n =5
+// for(let i=1;i<=n;i++){
+//   for(let j =1;j<=i;j++){
+//     process.stdout.write("* ")
+//   }
+//   console.log()
+// }
 
+
+// let n =5
+// for(let i =1;i<=n;i++){
+//   let chr = 97
+//   for(let j =1;j<=i;j++){
+//     process.stdout.write(String.fromCharCode(chr)+ " ")
+//     chr++
+//   }
+//   console.log();
+  
+
+// }
+
+// let n =5
+// for(let i =1;i<=n;i++){
+//   for(let j =1;j<=n-i+1;j++){
+//     process.stdout.write("*")
+//   }
+//   console.log();
+  
+// }
+// let n =5
+// for(let i =1;i<=n;i++){
+//   for(let j =1;j<=n-i+1;j++){
+//     process.stdout.write("*")
+//   }
+//   console.log();
+  
+// }
