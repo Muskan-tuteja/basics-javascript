a5ac1901f83b99152af1f3af495971a3965fd5cf
muskan <muskantuteja714@gmail.com>
2026-06-12 23:39:45 +0530

    basic js

--- company.js
+++ company.js
@@ -165,7 +165,43 @@
 // console.log(result);
 
 
+//  fetch th a data api call
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(respone => respone.json())
+// // .then(rej => rej.json())
+// .then(data=>{
+//   console.log(data);
+  
+// })
+// .catch(error=>{
+//   console.log("Error: ", error);
+  
+// })
 
 
 
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(response => response.json())
+// .then(data =>{
+//   console.log(data);
+  
+// })
+// .catch(error =>{
+//   console.log(error);
+  
+// })
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(response => response.json())
+// .then(data=>{
+//   console.log(data);
+  
+// })
+// .catch(error=>{
+//   console.log(error);
+  
+// })
 
